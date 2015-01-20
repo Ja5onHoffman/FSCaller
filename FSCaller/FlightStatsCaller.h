ae0f9d214e38a537f0172c9ff44cc40682eee03e
Ja5onHoffman <j@sonhoffman.com>
2015-01-20 14:04:38 -0500

    Update gitignore

--- FSCaller/FlightStatsCaller.h
+++ FSCaller/FlightStatsCaller.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  FlightStatsCaller.h
 //  FSCaller
 //
 //  Created by Jason Hoffman on 1/20/15.
 //  Copyright (c) 2015 JHM. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "AFHTTPSessionManager.h"
 
-@interface ViewController : UIViewController
-
+@interface FlightStatsCaller : AFHTTPSessionManager
 
 @end
-
---
+++ FSCaller/FlightStatsCaller.m
@@ -0,0 +1,13 @@
+//
+//  FlightStatsCaller.m
+//  FSCaller
+//
+//  Created by Jason Hoffman on 1/20/15.
+//  Copyright (c) 2015 JHM. All rights reserved.
+//
+
+#import "FlightStatsCaller.h"
+
+@implementation FlightStatsCaller
+
+@end
